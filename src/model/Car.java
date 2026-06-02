63f36e1e42b475d79c7c1e3a26dead707fc69985
Shum-dev <den.shumakovski@gmail.com>
2026-06-02 09:08:02 +0300

    Implemented Car builder pattern

--- src/model/Car.java
+++ src/model/Car.java
@@ -5,7 +5,7 @@ public class Car {
     private final int year;
     private final int power;
 
-    public Car(String model, int year, int power){
+    private Car(String model, int year, int power){
         this.model = model;
         this.year = year;
         this.power = power;
@@ -30,5 +30,30 @@ public class Car {
                 ", year=" + year +
                 ", power=" + power +
                 '}';
+    }
+
+    public static class Builder{
+        private String model;
+        private int year;
+        private int power;
+
+        public Builder setModel(String model){
+            this.model = model;
+            return this;
+        }
+
+        public Builder setYear(int year){
+            this.year = year;
+            return this;
+        }
+
+        public Builder setPower(int power){
+            this.power = power;
+            return this;
+        }
+
+        public Car build(){
+           return new Car(model, year, power);
+        }
     }
 }
