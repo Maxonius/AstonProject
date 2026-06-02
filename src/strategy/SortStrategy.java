4e00714a89c40d5af3a366593a78229535c01579
Maxim Malyshev <MaxMalysh2016@yandex.ru>
2026-06-02 20:56:24 +0300

    Sort interface + strategy
    Реализация интерфейса сортировки и паттерна Стратегия

---
+++ src/strategy/SortStrategy.java
@@ -0,0 +1,32 @@
+package strategy;
+
+import model.Car;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Интерфейс стратегии сортировки.
+ * Реализуется алгоритмом TimSort.
+ */
+public interface SortStrategy {
+    /**
+     * Базовая сортировка.
+     *
+     * @param cars список автомобилей.
+     * @param comparator компаратор, определяющий порядок сортировки.
+     */
+    void sort(List<Car> cars, Comparator<Car> comparator);
+
+    /**
+     * Сортировка с правилом четное/нечетное.
+     * Объекты с четным значением числового поля сортировки сортируются в натуральном порядке,
+     * а с нечетным - остаются на месте.
+     *
+     * @param cars список автомобилей.
+     * @param comparator компаратор, определяющий порядок сортировки четных элементов.
+     * @param number функция, извлекающая числовое поле (power или year).
+     */
+    void sortWithRule(List<Car> cars, Comparator<Car> comparator, Function<Car, Integer> number);
+}
