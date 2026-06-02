46298ad608c616ea3e54d47f1f0a1511727c8799
Shum-dev <den.shumakovski@gmail.com>
2026-06-02 10:09:12 +0300

    Added comparator tests

--- src/test/CarBuilderTest.java
+++ src/test/CarBuilderTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import comparator.PowerComparator;
 import model.Car;
 
 public class CarBuilderTest {
@@ -21,5 +22,15 @@ public class CarBuilderTest {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+
+        Car carAudi = new Car.Builder()
+                .setModel("Audi")
+                .setYear(2010)
+                .setPower(200)
+                .build();
+
+        PowerComparator comparator = new PowerComparator();
+        int result = comparator.compare(carBMW, carAudi);
+        System.out.println("Compare result: " + result);
     }
 }
