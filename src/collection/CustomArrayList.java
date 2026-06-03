9714bf815019cf4c51bb0d93f96fd72c4318bcd2
Shum-dev <den.shumakovski@gmail.com>
2026-06-03 08:55:18 +0300

    Implemented custom array list

---
+++ src/collection/CustomArrayList.java
@@ -0,0 +1,53 @@
+package collection;
+
+public class CustomArrayList<T> {
+    private Object[] elements;
+    private int size;
+
+    public CustomArrayList() {
+        this.elements = new Object[10];
+    }
+
+    public void add(T element) {
+        if (size == elements.length) {
+            grow();
+        }
+        elements[size] = element;
+        size++;
+    }
+
+    private void grow() {
+        int newCapacity = elements.length * 2;
+        Object[] newArray = new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = elements[i];
+        }
+        elements = newArray;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        return (T) elements[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
+        elements[size] = null;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
